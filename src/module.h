acfb08e2eaa78a900c87497c0c8e4fd55bdee915
pinkyroro10 <phone6.2545@gmail.com>
2024-09-02 12:40:34 -0700

    UP

--- src/module.h
+++ src/module.h
@@ -1,6 +1,8 @@
 #ifndef MODULE_H
 #define MODULE_H
 
+
+
 /*
 WiFi 3 Mode
 1 -> AP (Access Point)
@@ -8,13 +10,15 @@ WiFi 3 Mode
 3 -> APSTA (Access Point and Station)
 */
 /*------------------------------Start Lib--------------------------*/
-#include <Arduino.h>
 
+#include <Arduino.h>
 #include <WiFi.h>
 #include <SPIFFS.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
+#include <map>
+
 /*------------------------------End Lib-------------------------------*/
 
 
@@ -22,18 +26,54 @@ WiFi 3 Mode
 
 
 /*------------------------------Start variable--------------------------*/
+
 extern AsyncWebServer server;
 extern const char* ssid;
 extern const char* password;
+
+extern IPAddress ip;
+extern IPAddress gateways;
+extern IPAddress subnets;
+extern IPAddress dns;
+
+extern std::map<String, String> env_vars;
+
+extern String MODES;
+extern String AP_SSID;
+extern String AP_PSK;
+extern String STA_SSID;
+extern String STA_PSK;
+extern String mqtt_enable;
+extern String mqtt_broker;
+extern String mqtt_port;
+extern String mqtt_topic;
+extern String mqtt_username;
+extern String mqtt_password;
+extern String mqtt_interval;
+extern String RTU_enable;
+extern String RTU_deviceID;
+extern String RTU_baudRate;
+extern String TCP_wifi_enable;
+extern String TCP_eth_enable;
+extern String local_IP;
+extern String gateway;
+extern String subnet;
+extern String dnss;
+
 /*------------------------------END variable--------------------------*/
 
 
 
 
 /*------------------------------Start Function--------------------------*/
+void writEnvFile();
+void readEnvFile();
+void updateEnvVariable(const String &key, const String &value);
+bool configureNetwork(String IPAddressStr, String gatewayStr, String subnetStr, String dnsr);
+IPAddress stringToIPAddress(String ipStr);
+
 void setup_wifi();
 void setup_wifiAP();
-
 void Config_server();
 void handleIndex(AsyncWebServerRequest *request);
 
